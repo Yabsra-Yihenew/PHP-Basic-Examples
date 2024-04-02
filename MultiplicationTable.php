da2962729bc42d13b0be2dc467c9a3efbf8e299b
Yabsra Yihenew <yabsrayabsra2222@gmail.com>
2024-04-02 19:30:26 +0300

    Update MultiplicationTable.php

--- MultiplicationTable.php
+++ MultiplicationTable.php
@@ -1,1 +1,31 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border="2" style="cellpadding = 3px">
+    
+    <?php for($i=1 ; $i<=12 ; $i++)
+    {?>
+    <tr>
+        <?php 
+        for($j=1 ; $j<=12; $j++)
+        {?>
+            <td> 
+                <?php 
+                echo $i*$j ; ?>
+            </td>
+        <?php 
+        } ?>
+    </tr>
+
+    <?php
+    }?>
+
+    </table>
+</body>
+</html>
