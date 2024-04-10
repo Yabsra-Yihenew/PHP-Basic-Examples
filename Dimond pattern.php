23cee900ab2959edb2096bd9ddfaca8359f845ca
Yabsra Yihenew <yabsrayabsra2222@gmail.com>
2024-04-10 23:43:55 +0300

    Adding style

--- Dimond pattern.php
+++ Dimond pattern.php
@@ -1,10 +1,23 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Diamond Pattern</title>
+<style>
+  table {
+    border-collapse: collapse;
+    margin: auto;
+  }
+  td {
+    width: 20px;
+    height: 20px;
+    text-align: center;
+  }
+  .empty {
+    background-color: white;
+  }
+</style>
 </head>
 <body>
 
@@ -14,13 +27,13 @@
   for ($i = 1; $i <= $size; $i++) {
       echo "<tr>";
       for ($j = 1; $j <= $size - $i; $j++) {
-          echo "<td></td>";
+          echo "<td class='empty'></td>";
       }
       for ($k = 1; $k <= 2 * $i - 1; $k++) {
           echo "<td>*</td>";
       }
       for ($j = 1; $j <= $size - $i; $j++) {
-          echo "<td></td>";
+          echo "<td class='empty'></td>";
       }
       echo "</tr>";
   }
@@ -29,13 +42,13 @@
   for ($i = $size - 1; $i >= 1; $i--) {
       echo "<tr>";
       for ($j = 1; $j <= $size - $i; $j++) {
-          echo "<td></td>";
+          echo "<td class='empty'></td>";
       }
       for ($k = 1; $k <= 2 * $i - 1; $k++) {
           echo "<td>*</td>";
       }
       for ($j = 1; $j <= $size - $i; $j++) {
-          echo "<td></td>";
+          echo "<td class='empty'></td>";
       }
       echo "</tr>";
   }
