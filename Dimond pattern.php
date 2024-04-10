7f64363a4f75f2cff4b56adbaea602cbf2de8f32
Yabsra Yihenew <yabsrayabsra2222@gmail.com>
2024-04-10 23:45:31 +0300

    Adding comments

--- Dimond pattern.php
+++ Dimond pattern.php
@@ -5,15 +5,20 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Diamond Pattern</title>
 <style>
+  /* CSS styling for the table */
   table {
     border-collapse: collapse;
     margin: auto;
   }
+  
+  /* CSS styling for table cells */
   td {
     width: 20px;
     height: 20px;
     text-align: center;
   }
+  
+  /* CSS styling for empty cells */
   .empty {
     background-color: white;
   }
@@ -23,37 +28,55 @@
 
 <table border="1">
   <?php
-  $size = 5;
+  $size = 5; // Size of the diamond (number of rows)
+  
+  // Loop to create the upper half of the diamond
   for ($i = 1; $i <= $size; $i++) {
       echo "<tr>";
+      
+      // Adding empty cells before the asterisks
       for ($j = 1; $j <= $size - $i; $j++) {
           echo "<td class='empty'></td>";
       }
+      
+      // Adding asterisks
       for ($k = 1; $k <= 2 * $i - 1; $k++) {
           echo "<td>*</td>";
       }
+      
+      // Adding empty cells after the asterisks
       for ($j = 1; $j <= $size - $i; $j++) {
           echo "<td class='empty'></td>";
       }
+      
       echo "</tr>";
   }
   ?>
+  
   <?php
+  // Loop to create the lower half of the diamond
   for ($i = $size - 1; $i >= 1; $i--) {
       echo "<tr>";
+      
+      // Adding empty cells before the asterisks
       for ($j = 1; $j <= $size - $i; $j++) {
           echo "<td class='empty'></td>";
       }
+      
+      // Adding asterisks
       for ($k = 1; $k <= 2 * $i - 1; $k++) {
           echo "<td>*</td>";
       }
+      
+      // Adding empty cells after the asterisks
       for ($j = 1; $j <= $size - $i; $j++) {
           echo "<td class='empty'></td>";
       }
+      
       echo "</tr>";
   }
   ?>
 </table>
 
 </body>
-</html>
+  </html>
