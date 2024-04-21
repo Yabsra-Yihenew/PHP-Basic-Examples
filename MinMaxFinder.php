1ecf413829c77af4e285e0c502934402ad68a72f
Yabsra Yihenew <yabsrayabsra2222@gmail.com>
2024-04-21 22:42:38 +0300

    Update MinMaxFinder.php

--- MinMaxFinder.php
+++ MinMaxFinder.php
@@ -3,10 +3,12 @@
 <body>
 
 <?php
-echo(min(0, 150, 30, 20, -8, -200) . "<br>");
-echo(max(0, 150, 30, 20, -8, -200));
+$min_value = min(0, 150, 30, 20, -8, -200);
+$max_value = max(0, 150, 30, 20, -8, -200);
 ?>
+
+<p>Minimum Value: <?php echo $min_value; ?></p>
+<p>Maximum Value: <?php echo $max_value; ?></p>
 
 </body>
 </html>
-
